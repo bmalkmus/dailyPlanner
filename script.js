9dfe85343aa77f3771165e1e9ea44793336d0dd1
bmalkmus <bmalkmus23@gmail.com>
2020-01-15 18:23:30 -0800

    fixed my final bug with help of TA

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var now = moment().format("hA")
+var now = moment()
 console.log(now);
 let currentID;
 let inputID;
@@ -12,31 +12,35 @@ $("#current-date").html(currentDate);
 
 for (i = 0; i < timeArr.length; i++){
 time = timeArr[i];
-time = moment('\"'+time+'\"', 'HH').format('hA');
-    let row = $('<tr class =' + time + '></tr>');
-    let TDa = $('<td  class = "time-display" id =' + time + '>' + time + '</td>');
-    let TDb = $('<td><input type ="text" name ="' + time + '"  id ="text' + time + '"</td>')
+time = moment('\"'+time+'\"', 'HH');
+
+    console.log(time.isBefore(now))
+    strTime = time.format('hA')
+    let row = $('<tr class =' + strTime + '></tr>');
+    let TDa = $('<td  class = "time-display" id =' + strTime + '>' + strTime + '</td>');
+    let TDb = $('<td><input type ="text" name ="' + strTime + '"  id ="text' + time + '"</td>')
     let TDc = $('<button type="button"  class="btn btn-outline-info" id= "'+time+'">Save Task</button>')
     $(row).append(TDa, TDb, TDc);
     $('#mytable').append(row);
+    console.log({now, time, bool: now > time, moment: moment(now).isSameOrAfter(moment(time)) });
     
 
-    if (now > time){
-    $('input[name="'+time+'"]').css("background-color", "#cccccc");
+    if (now.isAfter(time, 'hour')){
+    $('input[name="'+strTime+'"]').css("background-color", "#cccccc");
     }
-    else if ( now == time){
-        $('input[name="'+time+'"]').css("background-color", "#f14d57");
+    else if (now.isSame(time, 'hour')){
+        $('input[name="'+strTime+'"]').css("background-color", "#f14d57");
     }
 
     else {
-        $('input[name="'+time+'"]').css("background-color", "#74e96d");
+        $('input[name="'+strTime+'"]').css("background-color", "#74e96d");
     }
 
     if (!localStorage.getItem("#text"+time)) {
-        $('input[name="'+time+'"]').val("")
+        $('input[name="'+strTime+'"]').val("")
     }
     else {
-        $('input[name="'+time+'"]').val(localStorage.getItem("#text"+time))
+        $('input[name="'+strTime+'"]').val(localStorage.getItem("#text"+time))
 }}
 
 $( "button" ).click(function() {
